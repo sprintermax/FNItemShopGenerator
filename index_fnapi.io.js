0fa70d588530f2df68bd79e3f0d7a96dfc94d6b5
Sprintermax <ericfnoe7@hotmail.com>
2022-11-17 10:37:25 -0300

    formated code

--- index_fnapi.io.js
+++ index_fnapi.io.js
@@ -1,127 +1,194 @@
-'use strict';
+"use strict";
 
-import fetch from 'node-fetch';
-import Jimp from 'jimp';
-import 'dotenv/config';
+import fetch from "node-fetch";
+import Jimp from "jimp";
+import "dotenv/config";
 
-import { shopItem as shopItemImage, finishProgram } from './src/utils.js';
+import { shopItem as shopItemImage, finishProgram } from "./src/utils.js";
 
 console.log("[INFO] Verificando os itens da loja");
 
-const shopData = await fetch('https://fortniteapi.io/v2/shop?lang=pt-BR', {
-    headers: {
-        Authorization: process.env.IO_API_TOKEN
-    }
-}).then(async res => {
+const shopData = await fetch("https://fortniteapi.io/v2/shop?lang=pt-BR", {
+  headers: {
+    Authorization: process.env.IO_API_TOKEN,
+  },
+})
+  .then(async (res) => {
     if (res.ok) return await res.json();
-    await finishProgram(`[ERROR] O Status Code recebido é direrente do esperado: ${res.status}`);
-}).catch(err => {
+    await finishProgram(
+      `[ERROR] O Status Code recebido é direrente do esperado: ${res.status}`
+    );
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const currentDate = shopData.lastUpdate.date.replace(" ", "-").split(`-`);
 const shopItems = shopData.shop;
 
 console.log(`[INFO] Loja verificada, ${shopItems.length} itens encontrados`);
 
-console.log('[INFO] Gerando imagem dos itens\n');
+console.log("[INFO] Gerando imagem dos itens\n");
 
-const missingItemImage = await Jimp.read('./src/images/QuestionMark.png');
-const largeItemOverlay = await Jimp.read('./src/images/LargeOverlay.png');
-const smallItemOverlay = await Jimp.read('./src/images/SmallOverlay.png');
-const shopBackground = await Jimp.read('./src/images/Background.png');
-const vbucksIcon = await Jimp.read('./src/images/VBucks.png');
+const missingItemImage = await Jimp.read("./src/images/QuestionMark.png");
+const largeItemOverlay = await Jimp.read("./src/images/LargeOverlay.png");
+const smallItemOverlay = await Jimp.read("./src/images/SmallOverlay.png");
+const shopBackground = await Jimp.read("./src/images/Background.png");
+const vbucksIcon = await Jimp.read("./src/images/VBucks.png");
 
-const titleFont = await Jimp.loadFont('./src/fonts/burbark/burbark_200.fnt');
-const dateFont = await Jimp.loadFont('./src/fonts/burbark/burbark_64.fnt');
-const burbankFont20 = await Jimp.loadFont('./src/fonts/burbark/burbark_20.fnt');
-const burbankFont16 = await Jimp.loadFont('./src/fonts/burbark/burbark_16.fnt');
+const titleFont = await Jimp.loadFont("./src/fonts/burbark/burbark_200.fnt");
+const dateFont = await Jimp.loadFont("./src/fonts/burbark/burbark_64.fnt");
+const burbankFont20 = await Jimp.loadFont("./src/fonts/burbark/burbark_20.fnt");
+const burbankFont16 = await Jimp.loadFont("./src/fonts/burbark/burbark_16.fnt");
 
 const itemPromises = [];
 
 shopItems.forEach((shopItem) => {
-    itemPromises.push(new Promise(async (resolve) => {
-        const firstItem = shopItem.granted[0];
-        const itemRarity = shopItem.rarity?.id || firstItem.rarity?.id;
-        const itemSeries = shopItem.series?.id || firstItem.series?.id;
-        let itemBackground;
-        let itemImage;
+  itemPromises.push(
+    new Promise(async (resolve) => {
+      const firstItem = shopItem.granted[0];
+      const itemRarity = shopItem.rarity?.id || firstItem.rarity?.id;
+      const itemSeries = shopItem.series?.id || firstItem.series?.id;
+      let itemBackground;
+      let itemImage;
 
-        try {
-            if (itemSeries) itemBackground = await Jimp.read(`./src/images/series/${itemSeries}.png`);
-            else itemBackground = await Jimp.read(`./src/images/rarities/${itemRarity}.png`);
-        } catch {
-            itemBackground = await Jimp.read('./src/images/rarities/Common.png');
-        }
+      try {
+        if (itemSeries)
+          itemBackground = await Jimp.read(
+            `./src/images/series/${itemSeries}.png`
+          );
+        else
+          itemBackground = await Jimp.read(
+            `./src/images/rarities/${itemRarity}.png`
+          );
+      } catch {
+        itemBackground = await Jimp.read("./src/images/rarities/Common.png");
+      }
 
-        try {
-            if (shopItem.mainType === "wrap") itemImage = await Jimp.read(firstItem.images.icon || firstItem.images.featured || shopItem.displayAssets[0].url);
-            else itemImage = await Jimp.read(shopItem.displayAssets[0].url || firstItem.images.icon);
-        } catch {
-            itemImage = missingItemImage;
-        }
+      try {
+        if (shopItem.mainType === "wrap")
+          itemImage = await Jimp.read(
+            firstItem.images.icon ||
+              firstItem.images.featured ||
+              shopItem.displayAssets[0].url
+          );
+        else
+          itemImage = await Jimp.read(
+            shopItem.displayAssets[0].url || firstItem.images.icon
+          );
+      } catch {
+        itemImage = missingItemImage;
+      }
 
-        itemBackground.resize(256, 256).blit(itemImage.resize(256, 256), 0, 0);
+      itemBackground.resize(256, 256).blit(itemImage.resize(256, 256), 0, 0);
 
-        const itemText = shopItem.displayName.toUpperCase();
-        const textHeight = Jimp.measureTextHeight(burbankFont20, itemText, 245);
-        const PriceWidth = 26 + 5 + Jimp.measureText(burbankFont20, `${shopItem.price.finalPrice}`);
+      const itemText = shopItem.displayName.toUpperCase();
+      const textHeight = Jimp.measureTextHeight(burbankFont20, itemText, 245);
+      const PriceWidth =
+        26 +
+        5 +
+        Jimp.measureText(burbankFont20, `${shopItem.price.finalPrice}`);
 
-        let priceTextPos;
+      let priceTextPos;
 
-        if (textHeight <= 22) {
-            itemBackground.blit(smallItemOverlay, 0, 0);
-            priceTextPos = 198;
-        } else {
-            itemBackground.blit(largeItemOverlay, 0, 0);
-            priceTextPos = 178;
-        }
+      if (textHeight <= 22) {
+        itemBackground.blit(smallItemOverlay, 0, 0);
+        priceTextPos = 198;
+      } else {
+        itemBackground.blit(largeItemOverlay, 0, 0);
+        priceTextPos = 178;
+      }
 
-        if (shopItem.mainType === 'bundle' || shopItem.granted.length >= 2) {
-            const subItemsText = `${shopItem.mainType === 'bundle' ? shopItem.granted.length : "+" + (shopItem.granted.length - 1)}`;
-            const subItemsTextWidth = Jimp.measureText(burbankFont16, subItemsText);
-            const subItemTag = new Jimp(subItemsTextWidth + 4, 20, 0x0);
-            subItemTag.print(burbankFont16, 2, 4, subItemsText);
-            itemBackground.blit(subItemTag, 243 - subItemsTextWidth, 226);
-        }
+      if (shopItem.mainType === "bundle" || shopItem.granted.length >= 2) {
+        const subItemsText = `${
+          shopItem.mainType === "bundle"
+            ? shopItem.granted.length
+            : "+" + (shopItem.granted.length - 1)
+        }`;
+        const subItemsTextWidth = Jimp.measureText(burbankFont16, subItemsText);
+        const subItemTag = new Jimp(subItemsTextWidth + 4, 20, 0x0);
+        subItemTag.print(burbankFont16, 2, 4, subItemsText);
+        itemBackground.blit(subItemTag, 243 - subItemsTextWidth, 226);
+      }
 
-        let priceTag = new Jimp(PriceWidth, 26, 0x0);
-        priceTag.blit(vbucksIcon.resize(26, 26), 1, 0);
+      let priceTag = new Jimp(PriceWidth, 26, 0x0);
+      priceTag.blit(vbucksIcon.resize(26, 26), 1, 0);
 
-        itemBackground.print(burbankFont20, 8, priceTextPos, {
-            text: itemText,
-            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
-        }, 240);
+      itemBackground.print(
+        burbankFont20,
+        8,
+        priceTextPos,
+        {
+          text: itemText,
+          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        },
+        240
+      );
 
-        priceTag.print(burbankFont20, 31, 5, {
-            text: shopItem.price.finalPrice.toString()
-        });
+      priceTag.print(burbankFont20, 31, 5, {
+        text: shopItem.price.finalPrice.toString(),
+      });
 
-        itemBackground.blit(priceTag, (128 - (PriceWidth / 2)), 220);
+      itemBackground.blit(priceTag, 128 - PriceWidth / 2, 220);
 
-        console.log(`Item pronto: "${itemText}"`)
-        resolve(new shopItemImage(itemText, shopItem.mainType === 'bundle', itemSeries, itemRarity, itemBackground));
-    }));
+      console.log(`Item pronto: "${itemText}"`);
+      resolve(
+        new shopItemImage(
+          itemText,
+          shopItem.mainType === "bundle",
+          itemSeries,
+          itemRarity,
+          itemBackground
+        )
+      );
+    })
+  );
 });
 
-const collumsCount = shopItems.length > 18 ? shopItems.length > 21 ? 8 : 7 : 6;
+const collumsCount =
+  shopItems.length > 18 ? (shopItems.length > 21 ? 8 : 7) : 6;
 
-shopBackground.resize(256 * collumsCount + 15 * (collumsCount - 1) + 100, 256 * Math.ceil(shopItems.length / collumsCount) + 15 * (Math.ceil(shopItems.length / collumsCount) - 1) + 350);
+shopBackground.resize(
+  256 * collumsCount + 15 * (collumsCount - 1) + 100,
+  256 * Math.ceil(shopItems.length / collumsCount) +
+    15 * (Math.ceil(shopItems.length / collumsCount) - 1) +
+    350
+);
 
-const titleText = 'LOJA DE ITENS';
-const leftWatermark = '';
-const rightWatermark = '';
+const titleText = "LOJA DE ITENS";
+const leftWatermark = "";
+const rightWatermark = "";
 const dateText = `DIA ${currentDate[2]}/${currentDate[1]}/${currentDate[0]}`;
 
 const titleWidth = Jimp.measureText(titleFont, titleText);
 const dateWidth = Jimp.measureText(dateFont, dateText);
 const watermarkWidth = Jimp.measureText(burbankFont20, rightWatermark);
 
-shopBackground.print(titleFont, ((shopBackground.bitmap.width / 2) - (titleWidth / 2)), 35, titleText);
-shopBackground.print(dateFont, ((shopBackground.bitmap.width / 2) - (dateWidth / 2)), 215, dateText);
+shopBackground.print(
+  titleFont,
+  shopBackground.bitmap.width / 2 - titleWidth / 2,
+  35,
+  titleText
+);
+shopBackground.print(
+  dateFont,
+  shopBackground.bitmap.width / 2 - dateWidth / 2,
+  215,
+  dateText
+);
 
-shopBackground.print(burbankFont20, 10, shopBackground.bitmap.height - 30, leftWatermark);
-shopBackground.print(burbankFont20, shopBackground.bitmap.width - watermarkWidth - 10, shopBackground.bitmap.height - 30, rightWatermark);
+shopBackground.print(
+  burbankFont20,
+  10,
+  shopBackground.bitmap.height - 30,
+  leftWatermark
+);
+shopBackground.print(
+  burbankFont20,
+  shopBackground.bitmap.width - watermarkWidth - 10,
+  shopBackground.bitmap.height - 30,
+  rightWatermark
+);
 
 let currentShopRow = 0;
 let currentShopColumn = 0;
@@ -130,25 +197,37 @@ let lastLineOffset = 0;
 const itemImages = await Promise.all(itemPromises);
 
 itemImages.sort((a, b) => {
-    const namePoints = a.itemName > b.itemName ? 1 : a.itemName < b.itemName ? -1 : 0;
-    return b.sortPoints - a.sortPoints + namePoints;
+  const namePoints =
+    a.itemName > b.itemName ? 1 : a.itemName < b.itemName ? -1 : 0;
+  return b.sortPoints - a.sortPoints + namePoints;
 });
 
-console.log('\n[INFO] Gerando imagem da loja');
+console.log("\n[INFO] Gerando imagem da loja");
 
 itemImages.forEach(({ image }) => {
+  if (
+    lastLineOffset === 0 &&
+    currentShopRow === Math.floor(itemImages.length / collumsCount)
+  )
+    lastLineOffset =
+      (256 * (collumsCount - (itemImages.length % collumsCount)) +
+        (collumsCount - (itemImages.length % collumsCount)) * 15) /
+      2;
 
-    if (lastLineOffset === 0 && currentShopRow === Math.floor(itemImages.length / collumsCount)) lastLineOffset = (256 * (collumsCount - itemImages.length % collumsCount) + (collumsCount - itemImages.length % collumsCount) * 15) / 2;
+  shopBackground.blit(
+    image,
+    lastLineOffset + 256 * currentShopColumn + 15 * currentShopColumn + 50,
+    256 * currentShopRow + 15 * currentShopRow + 300
+  );
 
-    shopBackground.blit(image, lastLineOffset + 256 * currentShopColumn + 15 * currentShopColumn + 50, 256 * currentShopRow + 15 * currentShopRow + 300);
-
-    if ((currentShopColumn + 1) % collumsCount === 0) {
-        currentShopRow += 1;
-        currentShopColumn = 0;
-    } else currentShopColumn += 1;
-
+  if ((currentShopColumn + 1) % collumsCount === 0) {
+    currentShopRow += 1;
+    currentShopColumn = 0;
+  } else currentShopColumn += 1;
 });
 
-shopBackground.write(`./ImagensGeradas/ItemShop-_${currentDate[2]}-${currentDate[1]}-${currentDate[0]}_FNAPI.io-${shopData.lastUpdate.uid}.png`);
+shopBackground.write(
+  `./ImagensGeradas/ItemShop-_${currentDate[2]}-${currentDate[1]}-${currentDate[0]}_FNAPI.io-${shopData.lastUpdate.uid}.png`
+);
 
-console.log('[INFO] Imagem da loja criada');
+console.log("[INFO] Imagem da loja criada");
--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,48 @@
-'use strict';
+"use strict";
 
-import config from './config.js';
+import config from "./config.js";
 
 class shopItem {
-    constructor(itemName, isBundle, series, rarity, image, isSpecial) {
-        this.itemName = itemName;
-        this.isBundle = isBundle ? true : false;
-        this.isSpecial = isSpecial ? true : false;
-        this.series = series ? series.toLowerCase() : null;
-        this.rarity = rarity.toLowerCase();
-        this.sortPoints = this.calcSortPoints();
-        this.image = image;
-    }
+  constructor(itemName, isBundle, series, rarity, image, isSpecial) {
+    this.itemName = itemName;
+    this.isBundle = isBundle ? true : false;
+    this.isSpecial = isSpecial ? true : false;
+    this.series = series ? series.toLowerCase() : null;
+    this.rarity = rarity.toLowerCase();
+    this.sortPoints = this.calcSortPoints();
+    this.image = image;
+  }
 
-    calcSortPoints() {
-        let points = 0;
+  calcSortPoints() {
+    let points = 0;
 
-        if (this.isSpecial) points += 10000 * config.sortpoints.special;
-        if (this.isBundle) points += 1000 * config.sortpoints.bundle;
-        if (this.series) points += 50 * (config.sortpoints.series[this.series] || config.sortpoints.series.otherseries);
-        if (this.rarity) points += 10 * (config.sortpoints.rarities[this.rarity] || config.sortpoints.series.otherrarity);
+    if (this.isSpecial) points += 10000 * config.sortpoints.special;
+    if (this.isBundle) points += 1000 * config.sortpoints.bundle;
+    if (this.series)
+      points +=
+        50 *
+        (config.sortpoints.series[this.series] ||
+          config.sortpoints.series.otherseries);
+    if (this.rarity)
+      points +=
+        10 *
+        (config.sortpoints.rarities[this.rarity] ||
+          config.sortpoints.series.otherrarity);
 
-        return points;
-    }
+    return points;
+  }
 }
 
 async function finishProgram(message) {
-    if (message) console.log(message);
-    console.log('\nPressione alguma tecla para finalizar');
-    process.stdin.setRawMode(true);
-    return new Promise(() => process.stdin.once('data', () => {
-        process.stdin.setRawMode(false);
-        process.exit();
-    }));
-};
-
-export {
-    shopItem,
-    finishProgram
+  if (message) console.log(message);
+  console.log("\nPressione alguma tecla para finalizar");
+  process.stdin.setRawMode(true);
+  return new Promise(() =>
+    process.stdin.once("data", () => {
+      process.stdin.setRawMode(false);
+      process.exit();
+    })
+  );
 }
+
+export { shopItem, finishProgram };
